22adb1f746236ac69c50ce595177abeaf7b92793
Manjunath Somawagol <SManjunath@quinnox.com>
2020-07-31 16:12:02 +0530

    Old code deleted

--- RunbookAPI/Interfaces/IMailService.cs
+++
@@ -1,8 +0,0 @@
-using System.Security.AccessControl;
-namespace RunbookAPI.Models
-{
-    public interface IMailService
-    {
-        System.Threading.Tasks.Task SendEmail(string toEmail,string subject,string body);
-    }
-}
--- RunbookAPI/Services/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNet.Identity.EntityFramework;
-
-namespace RunbookAPI.Services
-{
-    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
-    {
-
-    }
-}
